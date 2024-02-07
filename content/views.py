535c33c5f64bc2b67818664c3a373398ad86dfe3
hamal-astro <blingstarhamal@astrocamp.net>
2024-02-07 14:06:10 +0900

    clone_insta d5

--- content/views.py
+++ content/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed # .models 해당 앱 내의 모델
 
 # Create your views here.
+class Main(APIView):
+    def get(self,request):
+        feed_list = Feed.objects.all() 
+        # print(feed_list)
+        
+        for feed in feed_list:
+            print(feed.content)
+            
+        return render(request, 'clone_insta/main.html',context=dict(feeds=feed_list))
