0e7249a7bde3d266a9f84f4e8be97b26f881d495
hamal-astro <blingstarhamal@astrocamp.net>
2024-02-19 18:59:32 +0900

    clone_insta 8h13m

--- content/views.py
+++ content/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Feed # .models 해당 앱 내의 모델
+from user.models import User
 import os
 from uuid import uuid4
 from Clone_Insta.settings import MEDIA_ROOT
@@ -9,24 +10,31 @@ from Clone_Insta.settings import MEDIA_ROOT
 # Create your views here.
 class Main(APIView):
     def get(self,request):
-        feed_list = Feed.objects.all() 
-        # print(feed_list)
+        feed_list = Feed.objects.all().order_by('-id') #reverse
         
-        for feed in feed_list:
-            print(feed.content)
-            
-        return render(request, 'clone_insta/main.html',context=dict(feeds=feed_list))
+        email = request.session.get('email',None)
+
+        if email is None:
+            return render(request, 'user/login.html')
+
+        user = User.objects.filter(email=email).first()
+
+        if user is None:
+            return render(request, 'user/login.html')
+
+        return render(request, 'clone_insta/main.html', context=dict(feeds=feed_list, user=user))
+        
     
 
 class UploadFeed(APIView):
-    def post(self,request):
+    def post(self, request):
         
         file = request.FILES['file']
         
         uuid_name = uuid4().hex
         save_path = os.path.join(MEDIA_ROOT, uuid_name)
         
-        with open(save_path,'wb+') as destination:
+        with open(save_path, 'wb+') as destination:
             for chunk in file.chunks():
                 destination.write(chunk)
         
@@ -35,6 +43,10 @@ class UploadFeed(APIView):
         user_id = request.data.get('user_id')
         profile_image = request.data.get('profile_image')
         
-        Feed.objects.create(image=image,content=content,user_id=user_id,profile_image=profile_image,like_count=0)
+        Feed.objects.create(image=image, content=content, user_id=user_id, profile_image=profile_image, like_count=0)
         
         return Response(status=200)
+    
+class Profile(APIView):
+    def get(self,request):
+        return render(request,'content/profile.html')
--- user/views.py
+++ user/views.py
@@ -17,10 +17,10 @@ class Join(APIView):
         name = request.data.get('name', None)
         password = request.data.get('password', None)
         
-        User.object.create(email=email,nickname=nickname,name=name,password=make_password(password),profile_image='default_profile.jpg')
+        User.objects.create(email=email,nickname=nickname,name=name,password=make_password(password),profile_image='default_profile.jpg')
         
         return Response(status=200)
-    
+
 class Login(APIView):
     def get(self, request):
         return render(request, "user/login.html")
@@ -30,13 +30,20 @@ class Login(APIView):
         email= request.data.get('email', None)
         password = request.data.get('password', None)
         
-        user=User.object.filter(email=email).first()
+        user=User.objects.filter(email=email).first()
         
         if user is None:
             return Response(status=400,data=dict(message="가입하지 않거나, 잘못된 이메일 입니다."))
         
         if user.check_password(password):
             # 로그인을 했다. 세션이나 쿠키에 로그인정보 적용
+            request.session['email']=email
             return Response(status=200)
         else:
             return Response(status=400,data=dict(message="이메일이나 패스워드가 틀렸습니다."))
+        
+        
+class LogOut(APIView):
+    def get(self, request):
+        request.session.flush()
+        return render(request, "user/login.html")
